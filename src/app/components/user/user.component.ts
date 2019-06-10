b537d8d1722d2437d6cc20d225032f52f8fd024f
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-06-10 17:51:07 -0400

    Changed it to URL template.

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -2,6 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-user',
-  template: '<h2>John Doe</h2>'
+  templateUrl: './user.component.html'
 })
 export class UserComponent {}
