f1232f54afc390a834966d9e6482b9b222e7c492
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-06-10 20:44:10 -0400

    Added a user property, and imported the interface.

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../../Models/User';
 
 @Component({
   selector: 'app-users',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users.component.sass']
 })
 export class UsersComponent implements OnInit {
+  users: User;
+
   constructor() {
     console.log('From the constructor.');
   }
